b55f0eb5d25540a1024d148647712b45f23755c4
Jake Ascher <ascherj@gmail.com>
2019-01-15 11:22:02 -0500

    Escape message text with <script> tags

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -3,11 +3,28 @@ var Messages = {
   _storage: [],
 
   receive: (data) => {
-    Messages._storage = data.results.filter((message) => message.hasOwnProperty('username'));
+    Messages._storage = Messages.filter(data);
   },
 
   get: () => {
     return Messages._storage;
+  },
+
+  filter: (data) => {
+    let regex = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
+
+    // obtain messages from users
+    let userMessages = data.results.filter((message) => message.hasOwnProperty('username'));
+
+    // escape any text with <script> tags
+    let filteredUserMessages = userMessages.map((message) => {
+      if (regex.test(message.text)) {
+        message.text = '';
+      }
+      return message;
+    });
+
+    return filteredUserMessages;
   }
 
 };
