28627ceffc16ec8d669af28907af97d207c98b87
Jake Ascher <ascherj@gmail.com>
2019-01-16 11:31:45 -0500

    Update message HTML template

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -2,15 +2,9 @@ var MessageView = {
 
   render: _.template(`
       <div class="chat">
-        <div class="username">
-          <%= username %>
-        </div>
-        <div>
-          <%= text %>
-        </div>
-        <div class="timestamp">
-          <%= createdAt %>
-        </div>
+        <div class="username"><%= username %></div>
+        <div><%= text %></div>
+        <div class="timestamp"><%= createdAt %></div>
       </div>
     `)
 
