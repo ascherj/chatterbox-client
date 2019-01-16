d47c154b4eda9a4d2210d64ab57d9780376350b1
Jake Ascher <ascherj@gmail.com>
2019-01-16 11:48:46 -0500

    Add 'refresh messages' button and update click event

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,16 +1,23 @@
 var MessagesView = {
 
   $chats: $('#chats'),
+  $refreshButton: $('<button>Refresh Messages</button>'),
 
   initialize: function() {
     MessagesView.$chats.on('click', (event) => {
-      var user = $(event.target).text();
-      Friends.add(user);
+      if (event.target.className === 'username') {
+        var user = $(event.target).text();
+        Friends.add(user);
+      }
     });
+
+    MessagesView.$refreshButton.on('click', MessagesView.render);
   },
 
   render: function() {
     MessagesView.$chats.html('');
+    MessagesView.$chats.append(MessagesView.$refreshButton);
+
     var messages = Messages.get();
     messages = messages.filter(message => Rooms.isSelected(message.roomname));
     messages.forEach((message) => MessagesView.renderMessage(message));
